20dc42af4f8fb2875ba3a11eff14ebaa6219057e
haku <git@haku.me>
2015-06-25 22:49:02 +0100

    draws a single dubious shroom.

---
+++ README.md
@@ -0,0 +1,11 @@
+Faeri.es
+========
+
+WIP generated faerie landscale SVG.
+
+References
+----------
+
+https://github.com/wout/svg.js/
+http://www.w3.org/TR/SVG/paths.html#PathData
+https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/preserveAspectRatio
---
+++ faeries.js
@@ -0,0 +1,34 @@
+(function(){
+
+  document.addEventListener("DOMContentLoaded", function(event) {
+    drawFL()
+  })
+
+  var W = 1000
+  var H = 1000
+
+  var drawFL = function() {
+    var draw = SVG('bg0')
+      .size('100%', '100%')
+      .viewbox(0, 0, W, H)
+      .attr('preserveAspectRatio', 'xMidYMax slice')
+    draw.rect(W, H)
+      .fill({ opacity: 0 })
+      .stroke('#f0f')
+    shroom(draw)
+      .size(200, 200)
+      .cx(500).y(H - 200)
+  }
+
+  var shroom = function(draw) {
+    var m = draw.nested().viewbox(0,0,100,100)
+    m.rect(30, 80)
+      .cx(50).y(20)
+      .fill('#fff')
+    m.path('m 0,0 c 35,-20 65,-20 100,0 -50,-50 -50,-50 -100,0 z')
+      .cx(50).y(0)
+      .fill('#fff')
+    return m
+  }
+
+})()
