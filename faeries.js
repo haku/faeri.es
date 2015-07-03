a23914cf51813904ef9778efc3ca97e0464c03f4
haku <git@haku.me>
2015-07-03 22:21:12 +0100

    adjustable bottom indent.

--- faeries.js
+++ faeries.js
@@ -25,10 +25,19 @@
     m.rect(30, 80)
       .cx(50).y(20)
       .fill('#fff')
-    m.path('m 5,50 c 20,-10 70,-10 90,0 5,2 5,-1 -1,-7 C 50,0 50,0 6,43 0,49 0,52 5,50 z')
+    var btmIndent = -10 // 0 to -14.
+    var btmCornerTweak = (btmIndent < -5 ? 2 : 0)
+    m.path('m 5,50 ' +
+           'c 20,' + btmIndent + ' 70,' + btmIndent + ' 90,0 ' +
+           '5,' + btmCornerTweak + ' 5,-1 -1,-7 ' +
+           'C 50,0 50,0 6,43 ' +
+           '0,49 0,' + (50 + btmCornerTweak) + ' 5,50 z')
       .cx(50).y(0)
       .fill('#fff')
     return m
   }
+
+  // m 5,50 c 20,-10 70,-10 90,0 5,2 5,-1 -1,-7 C 50,0 50,0 6,43 0,49 0,52 5,50 z // start.
+  // m 5,50 c 20,-1  70,-1  90,0 5,0 5,-1 -1,-7 C 50,0 50,0 6,43 0,49 0,50 5,50 z // flatter bottom.
 
 })()
