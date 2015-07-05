e452e8e834cf2176e3e910da05551c87eb52c7c3
haku <git@haku.me>
2015-07-05 20:52:17 +0100

    falling leaves.

--- faeries.js
+++ faeries.js
@@ -11,7 +11,9 @@
   var drawFL = function() {
     var seed = rndInt(100000000, 999999999)
     console.log('seed', seed)
-    var rnd = sineDistFact(blumBlumShubFact(seed, 7247, 7823))
+    var bbs = blumBlumShubFact(seed, 7247, 7823)
+    var eRnd = evenDistFact(bbs)
+    var sRnd = sineDistFact(bbs)
 
     removeElementById('bg0_svg')
     var draw = SVG('bg0')
@@ -23,13 +25,21 @@
     draw.rect(W, H)
       .fill({ opacity: 0 })
 
-    var x = rnd(50, 100)
+    var x = sRnd(50, 100)
     while (x < 950) {
-      var h = rnd(50, 100)
-      shroom(draw, rnd)
+      var h = sRnd(50, 100)
+      shroom(draw, sRnd)
         .size(100, h)
         .cx(x).y(H - h)
-      x += rnd(50, 130)
+      x += sRnd(50, 130)
+    }
+
+    var leafCount = sRnd(10, 50)
+    for (i = 0; i < leafCount; i++) {
+      var h = sRnd(10, 20)
+      leaf(draw, sRnd)
+        .size(h, h)
+        .cx(eRnd(0, 1000)).cy(eRnd(0, 1000))
     }
   }
 
@@ -78,6 +88,28 @@
   // m 5,50 c 20,-10 70,-10 90,0 5,2 5,-1 -1,-7 C 50,0 50,0 6,43 0,49 0,52 5,50 z
   // m 65,30 c 0,0 0,17 0,35 0,17 0,35 0,35 l -30,0 c 0,0 0,-17 0,-35 0,-17 0,-35 0,-35 z
 
+  var leaf = function(draw, rnd) {
+    var stemW = rnd(0, 2)
+    var rotate = rnd(100, 260)
+
+    var l = draw.nested().viewbox(0, 0, 100, 100)
+
+    var lPath = ('m ' + (49 - stemW) + ',80 ' +
+      'c 0,15 0,15 ' + (1 + stemW) + ',15 ' +
+      (1 + stemW) + ',0 ' + (1 + stemW) + ',0 ' + (1 + stemW) + ',-15 ' +
+      'C ' + (85 + stemW) + ',60 85,45 50,5 ' +
+      '15,45 ' + (15 - stemW) + ',60 ' + (49 - stemW) + ',80 z')
+    l.path(lPath)
+      .cx(50).cy(50)
+      .attr('transform', 'rotate(' + rotate + ', 50, 50)')
+      .fill('#fff')
+
+    return l
+  }
+
+  // m 49,80  c 0,15 0,15 1,15  1,0 1,0 1,-15  C 85,60 85,45 50,5  15,45 15,60 49,80 z
+  // m 48,80  c 0,15 0,15 2,15  2,0 2,0 2,-15  C 86,60 85,45 50,5  15,45 14,60 48,80 z
+
   var removeElementById = function(id) {
     var el = document.getElementById(id)
     if (el) el.parentElement.removeChild(el)
@@ -91,6 +123,12 @@
     return function() {
       seed = (seed * seed) % (mod1 * mod2)
       return seed / (mod1 * mod2)
+    }
+  }
+
+  var evenDistFact = function(rnd) {
+    return function(min, max) {
+      return Math.floor(rnd() * (max - min)) + min
     }
   }
 
